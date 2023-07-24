4976ffc683d246a7221e336f2e173e3de8fe78bf
KofiGhana <abkarikari23@yahoo.co.uk>
2023-07-23 18:44:55 -0700

    update

---
+++ Printf_Test/power.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * get_flags - Calculates active flags
+ * @format: Formatted string in which to print the arguments
+ * @i: take a parameter.
+ * Return: Flags:
+ */
+int get_flags(const char *format, int *i)
+{
+	/* - + 0 # ' ' */
+	/* 1 2 4 8  16 */
+	int k, curr_a;
+	int flags = 0;
+	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+
+	for (curr_a = *a + 1; format[curr_a] != '\0'; curr_a++)
+	{
+		for (k = 0; FLAGS_CH[k] != '\0'; k++)
+			if (format[curr_a] == FLAGS_CH[k])
+			{
+				flags |= FLAGS_ARR[k];
+				break;
+			}
+
+		if (FLAGS_CH[k] == 0)
+			break;
+	}
+
+	 = curr_a - 1;
+
+	return (flags);
+}
