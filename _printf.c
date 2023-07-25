9944e121f1a8ffb9952f4cd38a1dd42c4d32e9da
KofiGhana <abkarikari23@yahoo.co.uk>
2023-07-25 04:19:12 -0700

    update

--- _printf.c
+++ _printf.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
-
 /**
- * _printf - Printf functions
- * @format: format.
- * Return: Printed chars.
+ * _printf - Functions of printf
+ * @format: format
+ * Return: Printed chars
  */
 int _printf(const char *format, ...)
+	void print_buffer(char buffer[], int *buff_ind);
 {
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
